8efa6526909373d15f4a3b8a58f9b0b094951c8f
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-11 02:17:08 +0100

    task 8.2 done

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,5 +1,16 @@
 import {test,expect} from '@playwright/test'
 
-test('This should be running', async () => {
+test('This should be running', async ({page}) => {
+    await page.goto('http://localhost:3000');
+    const textbox = page.getByRole("textbox") 
+    await textbox.fill('secret');              
+    await expect(textbox).toHaveValue('secret') 
+    
 
+})
+
+test('To do list', async ({page}) => {
+    await page.goto('http://localhost:3000');
+    const header = page.getByRole('heading', {name: "Todo List App"})
+    await expect(header).toBeVisible()
 })

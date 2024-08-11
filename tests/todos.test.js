4ea9e970c6d8a6a1b078a4195321dd8d2770773a
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-11 02:22:22 +0100

    structured better

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,9 +1,13 @@
 import {test,expect} from '@playwright/test'
 
 test('This should be running', async ({page}) => {
+    // ticket 4
     await page.goto('http://localhost:3000');
+    // ticket 5
     const textbox = page.getByRole("textbox") 
+    // ticket 6
     await textbox.fill('secret');              
+    // ticket 7
     await expect(textbox).toHaveValue('secret') 
     
 
@@ -11,6 +15,10 @@ test('This should be running', async ({page}) => {
 
 test('To do list', async ({page}) => {
     await page.goto('http://localhost:3000');
+    // task 8
+    // 2. see the "Todo List App" heading
     const header = page.getByRole('heading', {name: "Todo List App"})
     await expect(header).toBeVisible()
+    // 3. enter a todo into the input with the "New Todo:" label
+
 })
